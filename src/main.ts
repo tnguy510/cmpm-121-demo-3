6beffeeefb29f263cabd2d55a926f9b542ed5f40
Trish Nguyen <tnguy510@ucsc.edu>
2024-11-06 00:42:55 -0800

    initial commit check

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-// todo
+import "./style.css";
+
+const APP_NAME = "Cash Grab";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+const header = document.createElement("h1");
+
+document.title = APP_NAME;
+//app.innerHTML = APP_NAME;
+header.innerHTML = APP_NAME;
+app.append(header);
+
+const tmpButton = document.createElement("button");
+tmpButton.innerHTML = "this is a temp button";
+app.append(tmpButton);
+
+tmpButton.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
